692fef266387504c85e81c44c2700cdd7eae9343
Lindsey Kirkland <lindseykirkland4@gmail.com>
2021-07-05 20:35:17 -0400

    add 4.2

--- firstRepo.py
+++ firstRepo.py
@@ -23,3 +23,23 @@ if writing_tool and not has_graphite:
     print("it's a pen")
 if writing_tool and has_graphite:
     print("it's a pencil")
+
+#comparison operators
+# >(greater than), <(less than), >=(greater than or equal to), <=(less than or equal to), ==(equal to)
+#(!=), not equal to
+
+#Password check
+password_characters = 10
+if password_characters < 10:
+    print("Password needs at least 10 characters")
+elif password_characters > 50:
+    print("Password cannot exceed 50 characters")
+else:
+    print("Password looks great!")
+
+#len() returns the number of objects in a string
+
+pattern_types = ('stripes', 'floral', 'polka dots')
+pattern_types2 = ("stripes and floral and polka dots")
+print(len(pattern_types))
+print(len(pattern_types2))
--- practice.py
+++ practice.py
@@ -48,7 +48,7 @@ print("You will turn", age, "this year")
 
 weight = input('What is your weight in pounds? ')
 wconvert = int(weight) / 2.205
-print("you weigh ", wconvert, " kilograms")
+print("you weight is ", wconvert, " kilograms")
 
 #Ypu can use triple quotes to define a string for multiple lines"
 email = '''
@@ -173,3 +173,48 @@ def cred_check (credit, price):
 
 cred_check('bad', 15000)
 
+#using the AND operator
+#If we have multiple Booleans defined as True, we can write an and statement
+
+is_slimy = True
+has_Shell = False
+
+if is_slimy and has_Shell:
+    print("it's a snail")
+
+if is_slimy:
+    print("it's a slug")
+
+if is_slimy or has_Shell:
+    print("it's a mollusk")
+else: print("it's another animal")
+
+#in addition to 'and' and 'or', you can also use 'not' as a boolean value
+#use "and" to connect the statements and "not" to print the statement if it's false.
+writing_tool = True
+has_graphite = False
+
+if writing_tool and not has_graphite:
+    print("it's a pen")
+if writing_tool and has_graphite:
+    print("it's a pencil")
+
+#comparison operators
+# >(greater than), <(less than), >=(greater than or equal to), <=(less than or equal to), ==(equal to)
+#(!=), not equal to
+
+#Password check
+password_characters = 10
+if password_characters < 10:
+    print("Password needs at least 10 characters")
+elif password_characters > 50:
+    print("Password cannot exceed 50 characters")
+else:
+    print("Password looks great!")
+
+#len() returns the number of objects in a string
+
+pattern_types = ('stripes', 'floral', 'polka dots')
+pattern_types2 = ("stripes and floral and polka dots")
+print(len(pattern_types))
+print(len(pattern_types2))
